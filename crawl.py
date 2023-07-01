eef694e7153298a3756fffa568b5eef717a15d39
lvdthieu <luuvanducthieu291@gmail.com>
2023-07-01 17:27:50 +0700

    Change crawl data method

--- crawl.py
+++ crawl.py
@@ -4,18 +4,19 @@ import os
 from bs4 import BeautifulSoup
 from markdown import markdown
 import regex as re
+import numpy as np
 
 
-github_token = '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'
+github_token = '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'
 headers = {
-    'Authorization': f'token {github_token}',
+    'Authorization': f'token {github_token}',   
     'Accept': 'application/vnd.github.v3+json'
 }
 
-def crawl_release_notes(owner, repo):
+def crawl_changelogs(owner, repo):
     global headers
     page = 1
-    all_releases = []
+    all_changelogs = []
     while True:
         url = f'https://api.github.com/repos/{owner}/{repo}/releases?per_page=100&page={page}'
         response = requests.get(url, headers=headers)
@@ -23,22 +24,13 @@ def crawl_release_notes(owner, repo):
             print(response.status_code)
             print(response.text)
             break
-        releases = response.json()
-        releases_per_page = list(map(lambda release: release['body'], releases))
-        all_releases += releases_per_page
-        if len(releases) < 100:
+        changelogs = response.json()
+        changelogs_per_page = [changelog['body'] for changelog  in changelogs]
+        all_changelogs += changelogs_per_page
+        if len(changelogs) < 100:
             break
         page += 1
-    return all_releases
-
-
-def get_commit_message(commit):
-    commit_lines = commit.splitlines()
-    if len(commit_lines) == 0:
-        return '', ''
-    message = commit_lines[0]
-    description = ' '.join(commit_lines[1:])
-    return message, description
+    return all_changelogs
 
 
 def crawl_commit(owner, repo):
@@ -53,12 +45,33 @@ def crawl_commit(owner, repo):
             print(response.text)
             break
         commits = response.json()
-        commits_per_page = list(map(lambda commit: commit['commit']['message'], commits))
+        commits_per_page = [commit['commit']['message'] for commit in commits]
         all_commits += commits_per_page
         if len(commits) < 100:
             break
         page += 1
     return all_commits
+
+
+def get_commit(commit, replace=False):
+    if not commit:
+        return ('', [])
+    html = markdown(commit)
+    soup = BeautifulSoup(html, 'html.parser')
+    sentences = [p.text.stip() for p in soup.find_all('p')]
+    message = sentences[0]
+    description = sentences[1:]
+    description = '<.> '.join(description)
+    return message, description
+
+def get_release_note_sentences(release, replace=False):
+    if not release:
+        return []
+    html = markdown(release)
+    soup = BeautifulSoup(html, 'html.parser')
+    sentences = [li.text.strip() for li in soup.find_all('li')]
+    # print(sentences)
+    return sentences
 
 
 def markdown_to_text(markdown_string):
@@ -91,8 +104,6 @@ def markdown_to_text_abstract(markdown_string):
     text = ''.join(soup.findAll(string=True))
     return text
 
-
-
 if __name__ == '__main__':
     ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
     corpus_path = os.path.join(ROOT_DIR, 'data', 'Corpus Repo - Training.csv')
@@ -103,63 +114,56 @@ if __name__ == '__main__':
     print(corpus_repo_training.head())
     
     num_repo = corpus_repo_training.shape[0]
+
     for i in range(num_repo):
-        crawl_status = corpus_repo_training.loc[i, 'Crawl status']
-        if crawl_status != 'Done':
-            owner = corpus_repo_training.loc[i, 'User']
-            repo = corpus_repo_training.loc[i, 'Repo name']
-            folder_name = f'{owner}_{repo}'
-            print(owner, repo)
-            # Crawl release notes and commits
-            release_notes = crawl_release_notes(owner, repo)
-            print('Crawl releases done')
-            release_notes = [markdown_to_text(release) for release in release_notes]
-            release_notes_abstract = [markdown_to_text_abstract(release) for release in release_notes]
-            idx = range(1, len(release_notes) + 1)
-            release_notes_df = pd.DataFrame({'Index': idx, 'Release Note': release_notes, 
-                                             'Release Note Abstract': release_notes_abstract})
-            
-            commits = crawl_commit(owner, repo)
-            print('Crawl commits done')
-            commits = [markdown_to_text(commit) for commit in commits]
-            commits_abstract = [markdown_to_text_abstract(commit) for commit in commits]
-            messages = []
-            descriptions = []
-            messages_abstract = []
-            descriptions_abstract = []
-            for commit in commits:
-                message, description = get_commit_message(commit)
-                messages.append(message)
-                descriptions.append(description)
+        owner = corpus_repo_training.loc[i, 'User']
+        repo = corpus_repo_training.loc[i, 'Repo name']
+        folder_name = f'{owner}_{repo}'
+        print(owner, repo)
 
-            for commit in commits_abstract:
-                message_abstract, description_abstract = get_commit_message(commit)
-                messages_abstract.append(message_abstract)
-                descriptions_abstract.append(description_abstract)
+        # Crawl release notes and commits
+        release_notes = crawl_release_notes(owner, repo)
+        print('Crawl releases done')
 
-            idx = range(1, len(messages) + 1)
-            commits_df = pd.DataFrame({'Index': idx, 'Message': messages, 'Description': descriptions,
-                                       'Message Abstract': messages_abstract, 'Description Abstract': descriptions_abstract})
+        # Get release note sentences
+        release_notes_sentences = np.array([get_release_note_sentences(release_note) 
+                                            for release_note in release_notes])
+        release_notes_sentences = release_notes_sentences.flatten().reshape(-1, 1)
+        idx = np.array(range(1, len(release_notes) + 1))
+        
+        release_notes_df = pd.DataFrame(np.hstack((idx, release_notes_sentences)), 
+                                        columns=['Index', 'Release Note'])
+        
+        # Crawl commits
+        commits = crawl_commit(owner, repo)
+        print('Crawl commits done')
 
-            # Check dataframe
+        # Get commit messages and commit descriptions
+        commits = [get_commit_message(commit) for commit in commits]
+        messages, descriptions = zip(*commits)
+        idx = range(1, len(commits) + 1)
+        commits_df = pd.DataFrame({'Index': idx, 'Message': messages, 'Description': descriptions})
 
-            print(commits_df.shape)
-            print(release_notes_df.shape)
-            print(commits_df.head())
-            print(release_notes_df.head())
+        # Check dataframe
+        print(commits_df.shape)
+        print(release_notes_df.shape)
+        print(commits_df.head())
+        print(release_notes_df.head())
 
 
-            # Save data to folder
-            repo_folder_path = os.path.join(ROOT_DIR, 'data', folder_name)
+        # Save data to folder
+        repo_folder_path = os.path.join(ROOT_DIR, 'data', folder_name)
+        if not os.path.exists(repo_folder_path):
             os.mkdir(repo_folder_path)
-            release_notes_path = os.path.join(ROOT_DIR, 'data', folder_name, 'release_notes.csv')
-            commits_path = os.path.join(ROOT_DIR, 'data', folder_name, 'commits.csv')
-            release_notes_df.to_csv(release_notes_path, index=False)
-            commits_df.to_csv(commits_path, index=False)
 
-            corpus_repo_training.loc[i, 'Crawl status'] = 'Done'
-            corpus_repo_training.to_csv(corpus_path, index=False)
+        release_notes_path = os.path.join(ROOT_DIR, 'data', folder_name, 'release_notes.csv')
+        commits_path = os.path.join(ROOT_DIR, 'data', folder_name, 'commits.csv')
+        release_notes_df.to_csv(release_notes_path, index=False)
+        commits_df.to_csv(commits_path, index=False)
 
+        corpus_repo_training.loc[i, 'Crawl status'] = 'Done'
+        corpus_repo_training.to_csv(corpus_path, index=False)
+    
 
 
 
